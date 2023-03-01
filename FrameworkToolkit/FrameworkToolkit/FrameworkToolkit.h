569355847664cbc868dd94078acabaa4dd94abc8
nitpare <125563137+nitpare@users.noreply.github.com>
2023-03-01 15:23:44 +0530

    initial commit

---
+++ FrameworkToolkit/FrameworkToolkit/FrameworkToolkit.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkToolkit.h
+//  FrameworkToolkit
+//
+//  Created by Nitesh on 01/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkToolkit.
+FOUNDATION_EXPORT double FrameworkToolkitVersionNumber;
+
+//! Project version string for FrameworkToolkit.
+FOUNDATION_EXPORT const unsigned char FrameworkToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkToolkit/PublicHeader.h>
+
+
---
+++ FrameworkToolkit/FrameworkToolkit/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.swift
+//  FrameworkToolkit
+//
+//  Created by Nitesh on 01/03/23.
+//
+
+import Foundation
+
+public class HelloWorld {
+    public init() {} 
+    
+    public func printHelloWorld(message: String) {
+        print("Welcome: \(message)")
+    }
+}
